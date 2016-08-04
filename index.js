f7c8c777bea83f37286c49c2abc951f2055ebdc0
Jack <jack@Jacks-MacBook-Pro.local>
2016-08-04 18:35:12 -0400

    updating to attachments

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ function processData (d) {
 
 	//console.log(processed);
 	//console.log(noDate);
-	compilehtml(processed,noDate);
+	//compilehtml(processed,noDate);
 	compileAttachment(processed,noDate);
 
 	};
@@ -142,42 +142,36 @@ var testing = {
             "footer": "Slack API",
             "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
             "ts": 123456789
-        },
-        {
-            "fallback": "Required plain-text summary of the attachment.",
-            "color": "#36a64f",
-            "pretext": "Optional text that appears above the attachment block",
-            "author_name": "Bobby Tables",
-            "author_link": "http://flickr.com/bobby/",
-            "author_icon": "http://flickr.com/icons/bobby.jpg",
-            "title": "Slack API Documentation",
-            "title_link": "https://api.slack.com/",
-            "text": "Optional text that appears within the attachment",
-            "fields": [
-                {
-                    "title": "Priority",
-                    "value": "High",
-                    "short": false
-                }
-            ],
-            "image_url": "http://my-website.com/path/to/image.jpg",
-            "thumb_url": "http://example.com/path/to/thumb.png",
-            "footer": "Slack API",
-            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-            "ts": 123456789
         }
 
     ]
-}
+};
 
 
+var tasks = {
+	 "text" : "*Here's What I Found:*"
+};
+
+var attachmentsArray = [];
 
 function compileAttachment (p, n) {
+	
 	for (var i = 0 ; i <= 3; i++) {
-      
-      var task = "*" + p[i].name + "* _due "+ p[i].mom + "_ <" + p[i].link + "|View>\n";
-      html = html + task;
+		var aObj = {};
+		aObj.fallback = "Required plain-text summary of the attachment.";
+		aObj.color = "#2ecc71";
+		aObj.title = p[i].name;
+		aObj.title_link = p[i].link;
+		var fieldsArray = [];
+		var dueObj = {};
+		dueObj.title = "Due";
+		dueObj.value = p[i].mom;
+		fieldsArray.push(dueObj);
+		aObj.fields = fieldsArray;
+		attachmentsArray.push(aObj);
     };
+    tasks.attachments = attachmentsArray;
+    response.status(200).send(tasks);
 };
 
 
